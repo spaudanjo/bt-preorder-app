5a857a88e5ac06d1bf3fdcc9f277fc0091ebb5b9
Daniel Spaude <daniel@spaude.de>
2022-04-14 22:08:44 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+  const GlobalContext = React.createContext<{
+    language: string;
+    setLanguage?: React.Dispatch<React.SetStateAction<string>>;
+  }>({
+    language: "",
+  });
+const languages = [
+  {
+    name: "English",
+    id: "en",
+  },
+  {
+    name: "Deutsch",
+    id: "de",
+  },
+];
+
+const dictionaries = {
+  en: {
+    medicalFormTitle: "Medical Form",
+  },
+  de: {
+    medicalFormTitle: "Medizinisches Formular",
+  },
+};
+
+const Translate = ({ k: tKey }: { k: string }) => {
+  // TODO: change this for global language context
+  const language = "en";
+  const dictionary = dictionaries[language];
+  return <span>{tKey}</span>;
+};
+
+const formComponents = [
+  {
+    id: "medica-help",
+  },
+];
+
+
+const MedicalHelpForm = () => {
+  return (
+    <div>
+      <h1>
+        <Translate k="medicalFormTitle" />
+      </h1>
+      <p>This is a form for medical help.</p>
+    </div>
+  );
+};
+
+const LanguageSwitcher = () => {
+  const { language, setLanguage } = React.useContext(GlobalContext);
+  return (
+    <div>
+      {languages.map((language) => (
+        <button key={language.id}>{language.name}</button>
+      ))}
+    </div>
+  );
+};
 
 function App() {
+  const [language, setLanguage] = React.useState("en");
+
+  // const globalContext = React.createContext({language, setLanguage});
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <GlobalContext.Provider value={{language, setLanguage}}>
+      Test
+      <LanguageSwitcher />
+      <MedicalHelpForm />
+    </GlobalContext.Provider>
   );
 }
 
