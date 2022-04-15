01d098400b36702e5e93145f09ef6f31fb26c9a7
Daniel Spaude <daniel@spaude.de>
2022-04-15 08:53:01 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
 import React from "react";
 
-const languages = [
-  {
+const languages: { [key: string]: Language } = {
+  en: {
     name: "English",
     id: "en",
-  },
-  {
-    name: "Deutsch",
-    id: "de",
-  },
-];
-
-const dictionaries = {
-  en: {
-    medicalFormTitle: "Medical Form",
+    dictionary: {
+      medicalFormTitle: "Medical Help Form",
+    },
   },
   de: {
-    medicalFormTitle: "Medizinisches Formular",
+    name: "Deutsch",
+    id: "de",
+    dictionary: {
+      medicalFormTitle: "Medizinische Hilfe Formular",
+    },
   },
 };
 
+interface LanguageDictionary {
+  [key: string]: string;
+}
+
+interface Language {
+  name: string;
+  id: string;
+  dictionary: LanguageDictionary;
+}
+
 const Translate = ({ k: tKey }: { k: string }) => {
-  // TODO: change this for global language context
-  const language = "en";
-  const dictionary = dictionaries[language];
+  const { currentLanguage: language } = React.useContext(GlobalContext);
+  const dictionary = language.dictionary;
   return <span>{tKey}</span>;
 };
 
@@ -33,47 +39,49 @@ const formComponents = [
   },
 ];
 
-
 const MedicalHelpForm = () => {
-  return (
-    <div>
+  const { currentLanguage: language } = React.useContext(GlobalContext);
+  return (<div>
       <h1>
         <Translate k="medicalFormTitle" />
       </h1>
       <p>This is a form for medical help.</p>
+      Language: {language.name}
     </div>
   );
 };
 
 const LanguageSwitcher = () => {
-  const { language, setLanguage } = React.useContext(GlobalContext);
+  const { currentLanguage, setCurrentLanguage } = React.useContext(GlobalContext);
   return (
     <div>
-      {languages.map((language) => (
-        <button key={language.id} onClick={() => setLanguage(language.id)}>{language.name}</button>
-      ))}
+      {Object.keys(languages).map((languageKey) => {
+        const language = languages[languageKey];
+        return (
+          <button key={language.id} onClick={() => setCurrentLanguage(language)}>
+            {language.name}
+          </button>
+        );
+      })}
     </div>
   );
 };
 
+interface IGlobalContext {
+  currentLanguage: Language;
+  setCurrentLanguage: React.Dispatch<React.SetStateAction<Language>>;
+}
+const GlobalContext = React.createContext<IGlobalContext>({} as IGlobalContext);
 
-interface GlobalContext {
-  language: string;
-  setLanguage: React.Dispatch<React.SetStateAction<string>>;
-};
-const GlobalContext = React.createContext<GlobalContext>({} as GlobalContext);
+function GlobalContextProvider({ children }: { children: React.ReactNode }) {
+  const [currentLanguage, setCurrentLanguage] = React.useState(languages["en"]);
 
-function GlobalContextProvider({children}: {children: React.ReactNode}) {
-  const [language, setLanguage] = React.useState("en");
-
-  // const globalContext = React.createContext({language, setLanguage});
-  
   return (
-    <GlobalContext.Provider value={{language, setLanguage}}>
+    <GlobalContext.Provider value={{ currentLanguage, setCurrentLanguage }}>
       {children}
     </GlobalContext.Provider>
   );
-};
+}
 
 function App() {
   return (
