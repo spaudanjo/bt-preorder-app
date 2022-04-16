33cddcb8cf0c90e3f5da97506c49ec57da4b497a
Daniel Spaude <daniel@spaude.de>
2022-04-16 16:16:44 +0200

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import MedicalHelpForm from "./components/form-views/MedicalHelp";
 import { FlattenedFormViewResult, FormViewComponent, FormViewMappingEntry } from "./Types";
 import GlobalContextProvider, {
@@ -8,13 +8,6 @@ import GlobalContextProvider, {
 import languageMap from "./LanguageMap";
 import LanguageSwitcher from "./components/LanguageSwitcher";
 
-const formViewMapping: { [key: string]: FormViewMappingEntry } = {
-  "medical-help": {
-    id: "medical-help",
-    component: MedicalHelpForm,
-  },
-};
-
 const mockedFormStructureFromAPI = [
   {
     id: "medical-help",
@@ -22,9 +15,17 @@ const mockedFormStructureFromAPI = [
 ];
 
 function App() {
+  
+  const formViewMapping: { [key: string]: FormViewMappingEntry } = useMemo(() => ({
+    "medical-help": {
+      id: "medical-help",
+      component: MedicalHelpForm,
+    }
+  }), []);
+
   const [formViewIndex, setFormViewIndex] = React.useState(0);
-  const [formViewId, setFormViewId] = React.useState("");
-  const [formViewComponent, setFormViewComponent] = React.useState<FormViewComponent>();
+  // const [formViewId, setFormViewId] = React.useState("");
+  // const [formViewComponent, setFormViewComponent] = React.useState<FormViewComponent>();
   const [allFlattenedFormViewResults, setAllFlattenedFormViewResults] =
     React.useState<FlattenedFormViewResult>({});
 
@@ -43,21 +44,27 @@ function App() {
     setFormViewIndex((prevFormViewIndex: number) => prevFormViewIndex + 1);
   };
 
-  useEffect(() => {
-    setFormViewId(mockedFormStructureFromAPI?.[formViewIndex]?.id);
-    // const formViewId = mockedFormStructureFromAPI?.[formViewIndex]?.id;
-    // newFormViewComponent && setFormViewComponent(newFormViewComponent);
-    // alert(JSON.stringify(allFlattenedFormViewResults));
-  }, [formViewIndex]);
+  // useEffect(() => {
+  //   setFormViewId(mockedFormStructureFromAPI?.[formViewIndex]?.id);
+  //   // const formViewId = mockedFormStructureFromAPI?.[formViewIndex]?.id;
+  //   // newFormViewComponent && setFormViewComponent(newFormViewComponent);
+  //   // alert(JSON.stringify(allFlattenedFormViewResults));
+  // }, [formViewIndex]);
 
-    const newFormViewComponent =
-      !!formViewId && formViewMapping[formViewId]?.component;
+  // console.log('formViewIndex', formViewIndex);
+  const formViewId = mockedFormStructureFromAPI?.[formViewIndex].id;
+  console.log('formViewId', formViewId);
+  console.log('formViewMapping', formViewMapping);
+  // const newFormViewComponent =
+  const Component =
+    !!formViewId && formViewMapping[formViewId]?.component;
+  // console.log('newFormViewComponent', newFormViewComponent);
 
   return (
     <GlobalContextProvider languageMap={languageMap}>
       <LanguageSwitcher />
 
-      {/* {Component && <Component onSubmitFormView={onSubmitFormView} />} */}
+      {Component && <Component onSubmitFormView={onSubmitFormView} />}
       {/* {foo}
         <button onClick={() => setFormViewIndex(formViewIndex + 1)} >FOO</button> */}
 
