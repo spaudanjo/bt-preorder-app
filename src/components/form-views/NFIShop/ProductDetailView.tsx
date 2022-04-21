476f9502973d089c2f5985786a0e6c6d9169601f
Daniel Spaude <daniel@spaude.de>
2022-04-21 12:07:57 +0200

    WIP - work on Product Detail View

--- src/components/form-views/NFIShop/ProductDetailView.tsx
+++ src/components/form-views/NFIShop/ProductDetailView.tsx
@@ -8,40 +8,38 @@ import {
   Button,
   Modal,
   ModalBody,
-  ModalCloseButton,
   ModalContent,
   ModalFooter,
   ModalHeader,
   ModalOverlay,
-  useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
 import { GlobalContext } from "../../../GlobaContext";
-import { Product, ProductOrder } from "../../../Types";
+import { Product } from "../../../Types";
 import I18n from "../../I18n";
 import { getLocalizedProductDetailsForCurrentLanguageOrForEnglish } from "./helpers";
 
 interface ProductDetaiViewProps {
-  //   productType: string;
   productsForType: Product[];
-  // onAddToCart: (productOrders: ProductOrder[]) => void;
   onClose: () => void;
-  // isOpen: boolean;
 }
 const ProductDetailView = ({
-  // isOpen,
   onClose,
   productsForType,
-}: // onAddToCart,
+}:
 ProductDetaiViewProps) => {
   const { currentLanguage } = React.useContext(GlobalContext);
 
-  const localizedProductType = getLocalizedProductDetailsForCurrentLanguageOrForEnglish(productsForType[0], currentLanguage.id).productType
+  const localizedProductType =
+    getLocalizedProductDetailsForCurrentLanguageOrForEnglish(
+      productsForType[0],
+      currentLanguage.id
+    ).productType;
 
   const productsForTypeGroupedByGender = productsForType.reduce(
     (acc, product) => ({
-        ...acc,
-        [product.gender]: [...(acc[product.gender] || []), product],
+      ...acc,
+      [product.gender]: [...(acc[product.gender] || []), product],
     }),
     {} as { [key: string]: Array<Product> }
   );
@@ -52,35 +50,31 @@ ProductDetaiViewProps) => {
         <ModalHeader>{localizedProductType}</ModalHeader>
         {/* <ModalCloseButton /> */}
         <ModalBody>
-          {/* <p>
-            {JSON.stringify(productsForTypeGroupedByGender)}
-        </p>
-      PRODUCT DETAIL VIEW FOR PRODUCT TYPE {productsForType[0]?.productType} */}
           <Accordion defaultIndex={0} allowMultiple>
             {Object.keys(productsForTypeGroupedByGender).map((genderId) => {
               const productsForTypeAndGender =
                 productsForTypeGroupedByGender[genderId];
               return (
-            <AccordionItem key={genderId}>
-              <h2>
-                <AccordionButton>
-                  <Box flex="1" textAlign="left">
-                  <I18n k={`basics.${genderId}`} />
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-              {productsForTypeAndGender.map((product, i) => {
-                    const localizedProductDetails =
-                      getLocalizedProductDetailsForCurrentLanguageOrForEnglish(
-                        product,
-                        currentLanguage.id
-                      );
-                    return <p key={i}> {localizedProductDetails.size} </p>;
-                  })}
-              </AccordionPanel>
-            </AccordionItem>
+                <AccordionItem key={genderId}>
+                  <h2>
+                    <AccordionButton>
+                      <Box flex="1" textAlign="left">
+                        <I18n k={`basics.${genderId}`} />
+                      </Box>
+                      <AccordionIcon />
+                    </AccordionButton>
+                  </h2>
+                  <AccordionPanel pb={4}>
+                    {productsForTypeAndGender.map((product, i) => {
+                      const localizedProductDetails =
+                        getLocalizedProductDetailsForCurrentLanguageOrForEnglish(
+                          product,
+                          currentLanguage.id
+                        );
+                      return <p key={i}> {localizedProductDetails.size} </p>;
+                    })}
+                  </AccordionPanel>
+                </AccordionItem>
               );
             })}
           </Accordion>
